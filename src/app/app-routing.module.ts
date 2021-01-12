a18620d1688e599cf82b48028d9970cbba13ea9e
Walmiruff <walmiruff@gmail.com>
2021-01-11 21:20:36 -0300

    consertando adsense

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AvaAntropComponent } from './ava-antrop/ava-antrop.component';
 
 import { GastosEnergComponent } from './gastos-energ/gastos-energ.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { PlanAlimComponent } from './plan-alim/plan-alim.component';
 
 const routes: Routes = [
   {path: 'gastos-energ' , component: GastosEnergComponent},
   {path: 'ava-antrop', component: AvaAntropComponent },
   {path: 'plan-alim', component: PlanAlimComponent },
-  {path: 'not-found', component: NotFoundComponent},
   {path: '', redirectTo: 'gastos-energ', pathMatch: 'prefix'},
-  {path: '**', redirectTo: 'not-found' },
+  {path: '**', redirectTo: 'gastos-energ' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TextMaskModule } from 'angular2-text-mask';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { AdsenseModule } from 'ng2-adsense';
 import { NgSelectModule } from '@ng-select/ng-select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GastosEnergComponent } from './gastos-energ/gastos-energ.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { AvaAntropComponent } from './ava-antrop/ava-antrop.component';
 import { PlanAlimComponent } from './plan-alim/plan-alim.component';
 import { PrimOrSecOpPipe } from './shared/pipes/prim-or-sec-op.pipe';
@@ -20,7 +20,6 @@ import { environment } from '../environments/environment';
   declarations: [
     AppComponent,
     GastosEnergComponent,
-    NotFoundComponent,
     AvaAntropComponent,
     PlanAlimComponent,
     PrimOrSecOpPipe,
@@ -34,7 +33,10 @@ import { environment } from '../environments/environment';
     NgSelectModule,
     ModalModule.forRoot(),
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    AdsenseModule.forRoot({
+      adClient: 'ca-pub-3937154153155005',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
