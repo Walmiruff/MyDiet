2872f8e645a4888c60a5cc8b4c6bf28f6ae73c9f
Walmiruff <walmiruff@gmail.com>
2021-01-12 22:59:44 -0300

    suplementos ok

--- src/app/suplementos/suplementos-details/suplementos-details.component.ts
+++ src/app/suplementos/suplementos-details/suplementos-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-suplementos-details',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuplementosDetailsComponent implements OnInit {
 
-  constructor() { }
+  public routeParam: Number = -1;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+        this.routeParam = Number(params.get('id'));
+      });
   }
 
 }
--- src/app/suplementos/suplementos.module.ts
+++ src/app/suplementos/suplementos.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { SuplementosComponent } from './suplementos.component';
 import { SuplementosRoutingModule } from './suplementos-routing.module';
+import { SuplementosComponent } from './suplementos.component';
+import { SuplementosDetailsComponent } from '../suplementos/suplementos-details/suplementos-details.component';
 
 
 
 @NgModule({
-  declarations: [SuplementosComponent],
+  declarations: [
+    SuplementosComponent,
+    SuplementosDetailsComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
